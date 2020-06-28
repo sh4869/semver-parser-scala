2e3ca744f3b090cdd80fa514c79da245d99398f9
sh4869 <nobuk4869@gmail.com>
2020-06-29 01:50:36 +0900

    Add CommonParser

---
+++ src/main/scala/net/sh4869/semver_parser/CommonParser.scala
@@ -0,0 +1,66 @@
+package net.sh4869.semver_parser
+
+import scala.util.Try
+import scala.util.parsing.combinator._
+
+class CommonParser extends JavaTokenParsers {
+  def num_identifier: Parser[Int] = """(0|[1-9]\d*)""".r ^^ { _.toInt }
+
+  def prerelease_identifier: Parser[String] =
+    prerelease_identifier_content ~ rep("." ~> prerelease_identifier_content) ^^ {
+      case a ~ as => s"$a${if (as.isEmpty) "" else "." + as.mkString(".")}"
+    }
+
+  def prerelease_identifier_content: Parser[String] =
+    alphanumeric_identifier | num_identifier ^^ { _.toString }
+
+  def alphanumeric_identifier: Parser[String] =
+    """([a-zA-Z\-][0-9a-zA-Z\-]*)""".r ^^ { _.toString() } |
+      """([0-9a-zA-Z\-]*[a-zA-Z\-][0-9a-zA-Z\-]*)""".r ^^ { _.toString() }
+
+  def build_identifier: Parser[String] = build_identifier_content ~ rep("." ~> build_identifier_content) ^^ {
+    case a ~ as => s"$a${if (as.isEmpty) "" else "." + as.mkString(".")}"
+  }
+
+  def build_identifier_content: Parser[String] = """([a-zA-Z0-9\-]+)""".r ^^ { _.toString() }
+}
+
+object CommonParser {
+
+  /**
+    * compare preRelease for semver
+    * see also https://semver.org/#spec-item-11 .4
+    *
+    * @param preReleaseA
+    * @param preReleaseB
+    * @return
+    */
+  private[semver_parser] def comparePreRelease(preReleaseA: Option[String], preReleaseB: Option[String]): Int = {
+    (parsePR(preReleaseA), parsePR(preReleaseB)) match {
+      case (None, None)    => 0
+      case (Some(_), None) => -1
+      case (None, Some(_)) => 1
+      case (Some(thi), Some(tha)) => {
+        var result = 0
+        var i = 0
+        while (i < Seq(thi.length, tha.length).min && result == 0) {
+          (thi(i), tha(i)) match {
+            case (li: Long, la: Long) => result = li.compare(la)
+            case (si, sa)             => result = si.toString().compare(sa.toString())
+          }
+          i += 1
+        }
+        if (result != 0) result
+        else thi.length.compare(tha.length)
+      }
+    }
+  }
+
+  private def parsePR(opt: Option[String]) =
+    opt.map[Seq[_]](x =>
+      if (x.contains("."))
+        x.split("\\.").toIndexedSeq.map(x => Try { x.toLong }.getOrElse(x))
+      else Seq(x)
+    )
+
+}
--- src/main/scala/net/sh4869/semver_parser/SemVer.scala
+++ src/main/scala/net/sh4869/semver_parser/SemVer.scala
@@ -3,6 +3,8 @@ package net.sh4869.semver_parser
 import scala.util.Try
 import scala.util.parsing.combinator._
 
+import CommonParser._
+
 case class SemVer(
     major: Long,
     minor: Long,
@@ -18,60 +20,19 @@ case class SemVer(
     * see https://semver.org/#spec-item-11
     *
     * @param that
-    * @return 
+    * @return
     */
   def compare(that: SemVer): Int = {
     if (that.major != major) major.compare(that.major)
     else if (that.minor != minor) minor.compare(that.minor)
     else if (that.patch != patch) patch.compare(that.patch)
     else {
-      def parsePR(opt: Option[String]) =
-        opt.map[Seq[_]](x =>
-          if (x.contains("."))
-            x.split("\\.").toIndexedSeq.map(x => Try { x.toLong }.getOrElse(x))
-          else Seq(x)
-        )
-      (parsePR(preRelease), parsePR(that.preRelease)) match {
-        case (None, None)    => 0
-        case (Some(_), None) => -1
-        case (None, Some(_)) => 1
-        case (Some(thi), Some(tha)) => {
-          var result = 0
-          var i = 0
-          while (i < Seq(thi.length, tha.length).min && result == 0) {
-            (thi(i), tha(i)) match {
-              case (li: Long, la: Long) => result = li.compare(la)
-              case (si, sa)             => result = si.toString().compare(sa.toString())
-            }
-            i += 1
-          }
-          if (result != 0) result
-          else thi.length.compare(tha.length)
-        }
-      }
+      comparePreRelease(preRelease, that.preRelease)
     }
   }
 }
 
-private object SemVerParser extends JavaTokenParsers {
-  def num_identifier: Parser[Int] = """(0|[1-9]\d*)""".r ^^ { _.toInt }
-
-  def build_identifier: Parser[String] = build_identifier_content ~ rep("." ~> build_identifier_content) ^^ {
-    case a ~ as => s"$a${if (as.isEmpty) "" else "." + as.mkString(".")}"
-  }
-  def build_identifier_content: Parser[String] = """([a-zA-Z0-9\-]+)""".r ^^ { _.toString() }
-
-  def prerelease_identifier: Parser[String] =
-    prerelease_identifier_content ~ rep("." ~> prerelease_identifier_content) ^^ {
-      case a ~ as => s"$a${if (as.isEmpty) "" else "." + as.mkString(".")}"
-    }
-
-  def prerelease_identifier_content: Parser[String] =
-    alphanumeric_identifier | num_identifier ^^ { _.toString }
-
-  def alphanumeric_identifier: Parser[String] =
-    """([a-zA-Z\-][0-9a-zA-Z\-]*)""".r ^^ { _.toString() } |
-      """([0-9a-zA-Z\-]*[a-zA-Z\-][0-9a-zA-Z\-]*)""".r ^^ { _.toString() }
+private object SemVerParser extends CommonParser {
 
   def semver: Parser[SemVer] =
     num_identifier ~ ("." ~> num_identifier) ~ ("." ~> num_identifier) ~ ("-" ~> prerelease_identifier).? ~ ("+" ~> build_identifier).? ^^ {
